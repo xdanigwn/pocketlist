18faf0c723020090716ba6f4a0f6391138ac3413
Danigwn <danigwn@gmail.com>
2021-05-08 22:25:19 +0700

    update 2

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useState, useEffect, useCallback} from 'react'
 
 const AuthContext = createContext();
 
@@ -8,19 +8,28 @@ const AuthContext = createContext();
 function AuthContextProvider(props) {
     const [loggedIn, setLoggedIn] = useState(false) // STATE GLOBAL
     const [userId, setUserId] = useState("") // STATE GLOBAL
+    const [myToken, setMyToken] = useState("")
 
+    // async function getLoggedIn = useCallback(
+    //     () => {
+    //         callback
+    //     },
+    //     [input],
+    // )
 
-    async function getLoggedIn() { 
-        const loggedInRes = await axios.get("http://localhost:3000/api/v1/authcheck"); //run endpoint authcheck. return true / false
+    const getLoggedIn = useCallback(async () => {
+        const loggedInRes = await axios.get("https://admin-pocketlist.herokuapp.com/api/v1/authcheck"); //run endpoint authcheck. return true / false
         setUserId(loggedInRes.data.verif_user)
         setLoggedIn(loggedInRes.data.status) // fill with true/false = data loggedInRes
-    }
+        setMyToken(loggedInRes.data.mytoken)
+      }, [])
+
 
     useEffect(() => {
         getLoggedIn(); // when startup component, run function 
-    }, []);
+    }, [getLoggedIn]);
 
-    return <AuthContext.Provider value={{loggedIn, userId, getLoggedIn}}> 
+    return <AuthContext.Provider value={{userId, loggedIn,  myToken, getLoggedIn}}> 
         {props.children} 
     </AuthContext.Provider> // return value nya adalah nilai loggedin setelah di cek cookiesnya. children = router
 
