c8332ffb523f963c573dddd199a13c2a8d71a309
Danigwn <danigwn@gmail.com>
2021-05-11 02:31:30 +0700

    update 5

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useCallback, useContext } from "react";
+import React, { useEffect, useCallback,
+  // useContext 
+} from "react";
 import { connect } from "react-redux";
 import Header from "parts/Header";
 import Footer from "parts/Footer";
@@ -7,8 +9,8 @@ import Personal from "parts/Personal";
 import Account from "parts/Account";
 import moment from "moment";
 
-import AuthContext from "context/AuthContext"
-import { Redirect } from 'react-router';
+// import AuthContext from "context/AuthContext"
+// import { Redirect } from 'react-router';
 
 import { fetchPage } from "store/actions/page";
 // import PanelProduct from 'parts/PanelProduct'
@@ -19,32 +21,36 @@ function LandingPage(props) {
     const { page, fetchPage} = props;
     // const x = 1234
 
-    const { loggedIn, userId }  = useContext(AuthContext)
+    // const { loggedIn, userId }  = useContext(AuthContext)
     
     const refreshPage = useCallback(
       (dateFrom, dateTo) => {
         // console.log(userId)
-        fetchPage(`https://admin-pocketlist.herokuapp.com/api/v1/overview/${userId}/${dateFrom}/${dateTo}`, "landingPage");
+        fetchPage(`https://admin-pocketlist.herokuapp.com/api/v1/overview/5f6f68fc9fd56b291005a357/${dateFrom}/${dateTo}`, "landingPage");
       },
-      [fetchPage, userId],
+      [fetchPage, 
+        // userId
+      ],
     );
 
     useEffect (() => {
       // alert(loggedIn);
       // console.log(x)
-      if(userId){
+      // if(userId){
         refreshPage(moment().startOf("month").format("YYYY-MM-DD"), moment().format("YYYY-MM-DD"));  // SET FIRST LOAD WITH FIRST DATE OF MONTH - NOW DATE
-      }
+      // }
       // return () => {
       //   alert(loggedIn)
       // }
       
     // }, [refreshPage])
 
-  }, [refreshPage, userId])
+  }, [refreshPage, 
+    // userId
+  ])
   // console.log(page)
     if (!page.hasOwnProperty("landingPage")) return null;
-    if (loggedIn === false) return Redirect("/") // WHEN COMPONENT MOUNTED, CHECK LOGGED IN
+    // if (loggedIn === false) return Redirect("/") // WHEN COMPONENT MOUNTED, CHECK LOGGED IN
     // console.log(page.landingPage.accTransfer)
 
     return (
