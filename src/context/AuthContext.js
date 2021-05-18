0dcf081d41b7aacd8428d4e6c1adbed4f26f1174
Danigwn <danigwn@gmail.com>
2021-05-19 00:30:33 +0700

    update 8

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,15 +6,14 @@ const AuthContext = createContext();
 // ====== AUTHCHECK => SETLOGGEDIN = TRUE => PASS TO AUTHCONTEXT ========== // 
 
 function AuthContextProvider(props) {
-    const [loggedIn, setLoggedIn] = useState(false) // STATE GLOBAL
     const [userId, setUserId] = useState("") // STATE GLOBAL
+    const [name, setName] = useState("") // STATE GLOBAL
    
      const  getLoggedIn = useCallback( async () => {
       
         const loggedInRes = await axios.get("https://admin-pocketlist.herokuapp.com/api/v1/authcheck" ); //run endpoint authcheck. return true / false
         setUserId(loggedInRes.data.username)
-        setLoggedIn(true)
-      
+        setName(loggedInRes.data.fullname)
         
         // setLoggedIn(loggedInRes.data.status) // fill with true/false = data loggedInRes
         // setMyToken(loggedInRes.data.mytoken)
@@ -26,7 +25,7 @@ function AuthContextProvider(props) {
        
     }, [getLoggedIn]);
 
-    return <AuthContext.Provider value={{userId, loggedIn, getLoggedIn}}> 
+    return <AuthContext.Provider value={{userId, name, getLoggedIn}}> 
         {props.children} 
     </AuthContext.Provider> // return value nya adalah nilai loggedin setelah di cek cookiesnya. children = router
 
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -4,12 +4,13 @@ import IconLogo from "parts/IconLogo";
 import IconProfile from "parts/IconProfile";
 import axios from 'axios';
 import { useHistory } from 'react-router';
+import AuthContext from "context/AuthContext"
 // import { Link } from "react-router-dom";
 // import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 export default function Header(props) {
   const history = useHistory();
-
+  const { name }  = useContext(AuthContext)
   const getNavLinkClass = (path) => {
     return props.location.pathname === path ? " active" : "";
   };
@@ -58,7 +59,8 @@ export default function Header(props) {
                 </li> */}
               </ul>
               <div className="box-profile"><small><button className="btn btn-primary btn-user btn-small" onClick={Logout}>Logout</button></small>&nbsp;
-                <IconProfile />
+                {/* <IconProfile /> */}
+                Welcome, {name}
               </div>
             </div>
           </nav>
